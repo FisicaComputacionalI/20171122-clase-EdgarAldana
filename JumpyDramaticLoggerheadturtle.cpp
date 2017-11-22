672a43e1c59e9f54afabe7623f7478fab5ac5822
EdgarAldana <31289680+EdgarAldana@users.noreply.github.com>
2017-11-22 13:21:41 -0600

    Add files via upload
    Coseno de .15

---
+++ JumpyDramaticLoggerheadturtle.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+# include <math.h> 
+using namespace std; 
+int main() {
+  float res;
+  float res1;
+  float h;
+  float x;
+  x=0.15;
+  res=cos(x);
+cout << "cos(x)= "<<res<<endl;
+
+return (0);
+}
